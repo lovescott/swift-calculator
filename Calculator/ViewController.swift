b673fd34abc490fecd3808373f82384f2ed57def
Scott Love <scottlove@unboxedtechnology.com>
2015-04-18 02:12:21 -0400

    Lecture 2 Demo Code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController
 {
     
     @IBOutlet weak var display: UILabel!
-    var userIsInTheMiddleOfTypingANumber: Bool = false
     
-
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         
@@ -24,6 +25,51 @@ class ViewController: UIViewController
         else{
             display.text = digit
             userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    var displayValue: Double {
+        get{
+           return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "✖️": performOperation{$0 * $1}
+        case "➗": performOperation{$1 / $0}
+        case "➕": performOperation{$0 + $1}
+        case "➖": performOperation{$1 - $0}
+        case "√": performOperation{ sqrt($0) }
+        default: break
+        }
+    }
+    
+    private func performOperation (operation: (Double, Double) -> Double){
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    private func performOperation (operation: Double -> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
         }
     }
 }
