1b8f0c40902c138b9b0d4b4f914086f833788a84
Scott Love <scottlove@unboxedtechnology.com>
2015-04-25 13:28:42 -0400

    Added Program to Export Ops

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -41,7 +41,25 @@ class CalculatorBrain{
         knownOps["+"] = Op.BinaryOperation("+", +)
         knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
         knownOps["÷"] = Op.BinaryOperation("÷"){ $1 / $0 }
-        knownOps["÷"] = Op.UnaryOperation("√", sqrt)
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    var program: AnyObject {// guaranteed to be a PropertyList
+        get{
+            return opStack.map { $0.description}
+        }
+        set{
+            if let opSymbols = newValue as? Array<String>{
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    }else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue{
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+                opStack = newOpStack
+            }
+        }
     }
     
     private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]){
